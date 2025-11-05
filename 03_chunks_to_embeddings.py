28df7d60eaedd77b6e73ca12f8b1dd420dc57010
Jahid Ahmed Choudhury <chyjah8@gmail.com>
2025-11-05 20:05:31 +0530

    Update 03_chunks_to_embeddings.py

--- 03_chunks_to_embeddings.py
+++ 03_chunks_to_embeddings.py
@@ -13,6 +13,7 @@ import joblib
 
 def create_embedding(text_list):
     # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
+    # ollama local instance port
     r = requests.post("http://localhost:11434/api/embed", json={
         "model": "bge-m3",
         "input": text_list
