45874f9ebc89edf32c0aac20670b3a46dc41fe49
Jahid Ahmed Choudhury <chyjah8@gmail.com>
2025-11-05 19:36:16 +0530

    Update 01_process_video.py

--- 01_process_video.py
+++ 01_process_video.py
@@ -2,11 +2,6 @@
 import os
 import subprocess
 
-# files = os.listdir("videos")
-# for file in files:
-#     file_name= file.split(" ")[0]
-#     subprocess.run(["ffmpeg",f"videos/{file}", f"audios/{file_name}.mp3"])
-
 files = os.listdir("videos")
 for file in files:
  file_name = [os.path.splitext(file)[0] ]
