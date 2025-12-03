527a2e016432f019a4c8d52c3734879eb220e95f
Jahid Ahmed Choudhury <chyjah8@gmail.com>
2025-12-03 12:31:20 +0530

    Update 04_query_incoming.py

--- 04_query_incoming.py
+++ 04_query_incoming.py
@@ -42,12 +42,14 @@ max_indx = similarities.argsort()[::-1][0:top_results]
 new_df = df.loc[max_indx] 
 # print(new_df[["title", "number", "text"]])
 
-prompt = f'''I am teaching web development in my Sigma web development course. Here are video subtitle chunks containing video title, video number, start time in seconds, end time in seconds, the text at that time:
+prompt = f'''I am teaching web development in my Sigma web development course.
+Here are video subtitle chunks containing video title, video number, start time in seconds, end time in seconds, the text at that time:
 
 {new_df[["title", "number", "start", "end", "text"]].to_json(orient="records")}
 ---------------------------------
 "{incoming_query}"
-User asked this question related to the video chunks, you have to answer in a human way (dont mention the above format, its just for you) where and how much content is taught in which video (in which video and at what timestamp) and guide the user to go to that particular video. If user asks unrelated question, tell him that you can only answer questions related to the course
+User asked this question related to the video chunks, you have to answer in a human way (dont mention the above format, 
+its just for you) where and how much content is taught in which video (in which video and at what timestamp) and guide the user to go to that particular video. If user asks unrelated question, tell him that you can only answer questions related to the course
 '''
 with open("prompt.txt", "w") as f:
     f.write(prompt)
